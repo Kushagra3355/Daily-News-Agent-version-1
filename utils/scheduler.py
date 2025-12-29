032c66b612e7bf06667306cb9ac151d1db9ae8f7
Kushagra3355 <kushagra.omar@gmail.com>
2025-12-29 15:33:28 +0530

    updated the scheduler

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -6,13 +6,17 @@ import json, os
 
 def run_daily_job():
 
+    print("Getting the News from API...")
     api_resp = fetch_news()
+    print("Proprocessing the data...")
     news_data = preprocess_news(api_response=api_resp)
+    print("Summarizing the data...")
     summary = run_summarizer(news_data=news_data)
 
     # Save summary to JSON
     os.makedirs("data", exist_ok=True)
     with open("data/news_summary.json", "w") as f:
         json.dump(summary, f, indent=2)
+    print("Saved to JSON format.")
 
     return summary
