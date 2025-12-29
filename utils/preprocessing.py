1a7ef6718e8528ba0c76bc05c07b4889c6164fbd
Kushagra3355 <kushagra.omar@gmail.com>
2025-12-29 14:31:09 +0530

    removed the limit of preprocessing

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,15 +1,17 @@
-def preprocess_news(api_response: dict, limit: int = 5):
-    articles = api_response.get("results", [])[:limit]
+def preprocess_news(api_response: dict):
+    articles = api_response.get("results", [])
 
     cleaned = []
     for a in articles:
-        cleaned.append({
-            "title": a.get("title"),
-            "description": a.get("description"),
-            "category": a.get("category"),
-            "source": a.get("source_name"),
-            "date": a.get("pubDate"),
-            "url": a.get("link")
-        })
+        cleaned.append(
+            {
+                "title": a.get("title"),
+                "description": a.get("description"),
+                "category": a.get("category"),
+                "source": a.get("source_name"),
+                "date": a.get("pubDate"),
+                "url": a.get("link"),
+            }
+        )
 
     return cleaned
